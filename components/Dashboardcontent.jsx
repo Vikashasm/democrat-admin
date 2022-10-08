6384955319bfa00cd3f2bd4ab3862a05b4d4f768
Dikshaarora <dikshaarora6445@gmail.com>
2022-10-08 22:56:00 +0530

    dashboard content change

--- components/Dashboardcontent.jsx
+++ components/Dashboardcontent.jsx
@@ -1,59 +1,44 @@
-import Link from 'next/link';
-import React, { useState } from 'react';
-import Image from 'next/image';
+import Link from "next/link";
+import React, { useState } from "react";
+import Image from "next/image";
 
 function Dashboardcontent() {
   return (
     <>
-    <div className='dashboard_content'>
-<div className="row">
-   <div className=' box_one'>
-            <h1 className='heading'>Totals Users</h1>
-            <h5 className='sub_heading'>1951</h5>
-
-
+      <div className="dashboard_content">
+        <div className="row">
+          <div className=" box_one">
+            <h1 className="heading">Totals Users</h1>
+            <h5 className="sub_heading">1951</h5>
+          </div>
+          <div className=" box_one">
+            <h1 className="heading">Incomplete USers</h1>
+            <h5 className="sub_heading">33</h5>
+          </div>
+          <div className="box_one">
+            <h1 className="heading">Changed made by Users</h1>
+            <h5 className="sub_heading">35</h5>
+          </div>
+          <div className=" box_one">
+            <h1 className="heading">Manually added Products</h1>
+            <h5 className="sub_heading">0</h5>
+          </div>
+          <div className=" box_one">
+            <h1 className="heading">Comments Approval</h1>
+            <h5 className="sub_heading">3</h5>
+          </div>
+          <div className=" box_one">
+            <h1 className="heading">Contact reuests</h1>
+            <h5 className="sub_heading">6</h5>
+          </div>
+          <div className=" box_one">
+            <h1 className="heading"> Delete Users</h1>
+            <h5 className="sub_heading">2</h5>
+          </div>
         </div>
-         <div className=' box_one'>
-            <h1 className='heading'>Totals Users</h1>
-            <h5 className='sub_heading'>1951</h5>
-
-
-        </div>
-         <div className='box_one'>
-            <h1 className='heading'>Totals Users</h1>
-            <h5 className='sub_heading'>1951</h5>
-
-
-        </div>
-           <div className=' box_one'>
-            <h1 className='heading'>Totals Users</h1>
-            <h5 className='sub_heading'>1951</h5>
-
-
-        </div>
-         <div className=' box_one'>
-            <h1 className='heading'>Totals Users</h1>
-            <h5 className='sub_heading'>1951</h5>
-
-
-        </div>
-         <div className=' box_one'>
-            <h1 className='heading'>Totals Users</h1>
-            <h5 className='sub_heading'>1951</h5>
-
-
-        </div>
-         <div className=' box_one'>
-            <h1 className='heading'>Totals Users</h1>
-            <h5 className='sub_heading'>1951</h5>
-
-
-        </div>
-</div>
-     
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Dashboardcontent
+export default Dashboardcontent;
